80772e84ac4d66ff409c4a914a89cfa9b0a67970
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2026-02-16 19:26:25 +0200

    qwert

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import { createGlobalStyle } from "styled-components";
 import "modern-normalize";
-
 import { Section } from './components/Section/Section';
 import { Tittle } from './components/Tittle/Tittle';
 import { CardList } from './components/CardList/CardList';
@@ -7,20 +7,44 @@ import { Ivent } from './components/Ivent/Ivent';
 import './App.css';
 import games from "./games.json"
 import { Button } from "./components/Button/Button";
-import SubTitle from "./SubTitle/SubTitle";
+import SubTitle from "./components/SubTitle/SubTitle";
+
+
+
+const GlobalStyle = createGlobalStyle`
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+
+}
+
+body{
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  background-color: gray;
+}
+
+
+`
+
+
 
 function App() {
   return (
-    <Section>
-      <Tittle text="Найпопулярніші ігри" />
-      <SubTitle text="Може навіть найкраші"/>
-      <CardList>
-        <Ivent props={games} />
-      </CardList>
-      <Button text="Open" success  />
-      <Button text="Close" error />
-      <Button text="Warning" info/>
-    </Section>
+    <>
+      <GlobalStyle />
+      <Section>
+        <Tittle text="Найпопулярніші ігри" />
+        <SubTitle text="Може навіть найкраші" />
+        <CardList>
+          <Ivent props={games} />
+        </CardList>
+        {/* <Button text="Open" success />
+        <Button text="Close" error />
+        <Button text="Warning" info /> */}
+        <Button text="Hello world"/>
+      </Section>
+    </>
   );
 }
 
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,41 @@
 import clsx from "clsx"
-import style from"./Button.module.css"
+import style from "./Button.module.css"
+import styled from "styled-components";
+
+const Buton = styled.button`
+  color: #bf4f74;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`;
+
+
+const TomatoButton = styled(Buton)`
+  color: tomato;
+  border-color: tomato;
+background-color: blue;
+`;
+
 export const Button = ({ text, success, error, info }) => {
   return (
-    <button
-      className={clsx(style.btn, {
-        [style.success]: success,
-        [style.error]: error,
-        [style.info]: info
-      })}
-      type="button"
-    >
-      {text}
-    </button>
+    // <button
+    //   className={clsx(style.btn, {
+    //     [style.success]: success,
+    //     [style.error]: error,
+    //     [style.info]: info
+    //   })}
+    //   type="button"
+    // >
+    //   {text}
+    // </button>
+    <>
+      <Buton>{text}</Buton>
+      <TomatoButton>{ text}</TomatoButton>
+    </>
   );
 };
+
+
+
